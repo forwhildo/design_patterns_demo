e07145dfe7f6a3d4e9faab7d666e8bc16d1c25dc
forwhildo <2750419070@qq.com>
2023-05-22 18:12:58 +0800

    feature:原型模式，深拷贝和浅拷贝

---
+++ prototype/src/demo/MyBeanUtils.java
@@ -0,0 +1,71 @@
+package demo;
+
+//import org.springframework.beans.BeanUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MyBeanUtils {
+
+
+
+    /**
+     * 浅拷贝
+     * 将包含BeanA的list，转换为包含BeanB的list。应用场景示例：List<DO>转List<TO>
+     *
+     * @param sourceList
+     * @param targetClass
+     * @return
+     */
+    public static <T> List<T> copyList(List<?> sourceList, Class<T> targetClass) {
+        if (sourceList == null) {
+            return null;
+        }
+
+        List<T> targetList = new ArrayList<>();
+
+        for (Object tempSrc : sourceList) {
+            try {
+                T t = copyBean(tempSrc, targetClass);
+                targetList.add(t);
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return targetList;
+    }
+
+    /**
+     * 浅拷贝,封装spring的 BeanUtils.copyProperties()
+     *
+     * @param srcBean     待转换Bean
+     * @param targetClass 目标Bean的Class
+     * @param <T>         目标Bean
+     * @return
+     */
+    public static <T> T copyBean(Object srcBean, Class<T> targetClass) {
+        try {
+            T t = targetClass.newInstance();
+//            BeanUtils.copyProperties(srcBean, t);
+            return t;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    /**
+     * 深拷贝
+     */
+    public static <T> List<T> deepCopy(List<T> src) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+        out.writeObject(src);
+
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+
+        List<T> dest = (List<T>) in.readObject();
+        return dest;
+    }
+}
